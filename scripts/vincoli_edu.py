ca2ed129c7f3b1bf34c5f3891aec00b57eb916a4
simoLoc <simonaloc1@yahoo.it>
2025-01-10 17:41:36 +0100

    Modifiche varie

--- scripts/vincoli_edu.py
+++ scripts/vincoli_edu.py
@@ -3,25 +3,22 @@ from collections import Counter, defaultdict
 import matplotlib.pyplot as plt
 import networkx as nx
 
-# Funzione per caricare il dataset STAC
+
 def load_data(file_path):
     with open(file_path, 'r') as f:
         return json.load(f)
 
 # Funzione per analizzare gli EDUs
-def analyze_edus(data):
-    edu_lengths = []
+def get_all_edus_lengths(data):
     text_lengths = []
     for dialogue in data:
         for turn in dialogue.get('edus'):
             edu_text = turn.get('text')
             text_lengths.append(len(edu_text))
-            # for edu in edus:
-            #     edu_lengths.append(len(edu.split()))
     return text_lengths
 
-# Funzione per ottenere EDUs uniche
-def get_unique_edus(data):
+# Funzione che restituisce la lista di tutte le EDUs contenute nel dataset
+def get_edus_list(data):
     unique_edus = set()
     for dialogue in data:
         for turn in dialogue.get('edus', []):
@@ -29,8 +26,9 @@ def get_unique_edus(data):
             unique_edus.add(edu_text)
     return list(unique_edus)
 
-# Funzione per ottenere EDUs uniche con un contatore
-def get_edus_with_counts(data):
+# Funzione che restituisce una lista di coppie (t, c) in cui t rappresenta la singola EDU
+# e c è il numero di volte che la EDU t appare nel dataset
+def get_edus_counter_list(data):
     edu_counter = Counter()
     for dialogue in data:
         for turn in dialogue.get('edus', []):
@@ -38,19 +36,19 @@ def get_edus_with_counts(data):
             edu_counter[edu_text] += 1
     return sorted(edu_counter.items(), key=lambda item: item[1], reverse=True)
 
-# Main function to perform analysis
+
 def main():
     file_path = "dataset/STAC/train_subindex.json"
     data = load_data(file_path)
 
-    edu_lengths = analyze_edus(data)
-    edus_with_counts = get_edus_with_counts(data)
+    edus_lengths = get_all_edus_lengths(data)
+    edus_counter_list = get_edus_counter_list(data)
 
-    print(f"Number of EDUs: {len(edu_lengths)}")
-    print(f"Average EDU length: {sum(edu_lengths) / len(edu_lengths):.2f}")
-    print(f"Number of unique EDUs: {len(edus_with_counts)}")
+    print(f"Number of EDUs: {len(edus_lengths)}")
+    print(f"Average EDU length: {sum(edus_lengths) / len(edus_lengths):.2f}")
+    print(f"Number of unique EDUs: {len(edus_counter_list)}")
     print("Sample of EDUs with counts:")
-    print(edus_with_counts[:785])
+    print(edus_counter_list[:785])
 
 if __name__ == "__main__":
     main()
